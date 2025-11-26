b53a109f62cd5ccbe7e6cfde9f1516efccbfdf75
codemmk <2107228359@qq.com>
2025-11-27 07:33:10 +0800

    feat: Modify path

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex h-full">
+  <div class="layout-left"></div>
+  <div class="layout-right">
+    <header></header>
+    <div>
+      <div>
+        <main></main>
+        <!-- layoutside -->
+        <div></div>
+      </div>
+      <footer></footer>
+    </div>
+  </div>
+</div>
