8d48d155091cb5c64d1ca8d40d3887aa091c3ae1
codemmk <2107228359@qq.com>
2025-11-28 13:09:10 +0800

    feat: login path change

--- client/src/lib/components/ui/Menu.svelte
+++ client/src/lib/components/ui/Menu.svelte
@@ -2,7 +2,7 @@
   <nav>
     <ul class="flex flex-col gap-2 p-4">
       <li><a href="/">Home</a></li>
-      <li><a href="/login">Login</a></li>
+      <li><a href="/auth/login">Login</a></li>
       <li><a href="/admin">Admin</a></li>
     </ul>
   </nav>
