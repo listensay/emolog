e5f4c98a86bbe2ef2296b4f6a84938e62d5a30f3
listensay <odr233@gmail.com>
2026-02-04 13:35:32 +0800

    feat: 统一登录检测，删除多余检测代码

--- client/src/routes/admin/+layout.ts
+++ client/src/routes/admin/+layout.ts
@@ -1,12 +1,12 @@
 import { redirect } from '@sveltejs/kit';
-import type { PageLoad } from './$types';
+import type { LayoutLoad } from './$types';
 
-export const load: PageLoad = async () => {
+export const load: LayoutLoad = async () => {
 	// 检查是否已登录
 	if (typeof window !== 'undefined') {
 		const token = localStorage.getItem('token');
 		if (!token) {
-			// 未登录,重定向到登录页面
+			// 未登录，重定向到登录页面
 			throw redirect(302, '/auth/login');
 		}
 	}
